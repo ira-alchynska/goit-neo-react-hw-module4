478d5f90f70e1d3c029ec6e30dc731a09944bf8d
Iryna Alchynska <iren0988@gmail.com>
2024-11-29 20:20:38 +0100

    fix: import

--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -1,6 +1,6 @@
 import PropTypes from 'prop-types';
 import styles from './ImageGallery.module.css';
-import ImageCard from './ImageCard';
+import ImageCard from '../ImageCard/ImageCard';
 
 const ImageGallery = ({ images, onImageClick }) => (
     <ul className={styles.gallery}>
